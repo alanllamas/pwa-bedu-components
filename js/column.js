cdf7ff5dd4918327f62b5d0d4647473646cf8bbb
alanllamas <alan@alans-MacBook-Pro.local>
2018-10-04 18:24:05 -0500

    modified column to use as grid and added accordion

--- js/column.js
+++ js/column.js
@@ -1,15 +1,37 @@
 // var columns = document.getElementById('columns');
 // var create = document.getElementById('create');
+// var resizing = false ;
 var container = document.getElementById('column-container');
 
-// create.onclick = () => {
+colNum = 5
+colArrange = 3
+container.innerHTML  = "";
+for (var i = 0; i < colNum; i++) {
+    console.log(i);
 
-    container.innerHTML  = "";
-    for (var i = 0; i < 3; i++) {
-        console.log(i);
-        container.innerHTML += `<div id="column-${i + 1}" class="content-column">
-        <h4 class="title">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h4>
-        <p class="content">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        </div>`;
+    console.log('container.outerWidth',container.outerWidth);
+    container.innerHTML += `<div id="column-${i + 1}" class="content-column">
+    <div class="title">
+    <!--
+     -->
+     <img src="https://placehold.it/50x50?text=icon" alt="">
+    <h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h4>
+    </div>
+    <p class="content">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </div>`;
+    var column = document.getElementsByClassName('content-column')
+    // var title = document.getElementsByClassName('title').querySelector('h4')
+    meassureWidth = () => {
+        if (window.innerWidth >= 768) {
+            // container.outerWidth = ((100/colNum) + '%');
+            // title[i].style.width = '75%';
+            column[i].style.width = ((100/colArrange) + '%');
+        }
+        else if (window.innerWidth <= 768) {
+            // container.outerWidth = '100%';
+            column[i].style.width = '100%';
+        }
+        console.log(column[i]);
     }
-// }
+    meassureWidth();
+}
---
+++ js/iint.js
@@ -0,0 +1,31 @@
+fold = (e) => {
+    var option = document.getElementsByClassName('option')
+    for (var i = 0; i < option.length; i++) {
+        if (i === e) {
+            if (option[i].nextSibling.nextSibling.style.display == 'none') {
+                option[i].nextSibling.nextSibling.style.display = 'block'
+                option[i].children[1].className = 'fa fa-chevron-up'
+            }else {
+                option[i].nextSibling.nextSibling.style.display = 'none'
+                option[i].children[1].className = 'fa fa-chevron-down'
+            }
+        }else{
+            option[i].nextSibling.nextSibling.style.display = 'none'
+            option[i].children[1].className = 'fa fa-chevron-down'
+        }
+    }
+}
+var container = document.getElementById('iint-container');
+
+for (var i = 0; i < 2; i++) {
+    container.innerHTML += `
+        <div class="container">
+            <div onclick="fold(${i})" class="option">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat animi saepe quod!</p>
+                <i class="fa fa-chevron-down"></i>
+            </div>
+            <div class="content" style="display:none">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam cum facilis veritatis autem amet, quaerat tenetur dicta mollitia hic obcaecati, asperiores, officiis maxime. Voluptas commodi ex neque debitis tempora sapiente.</p>
+            </div>
+        </div>`
+}
